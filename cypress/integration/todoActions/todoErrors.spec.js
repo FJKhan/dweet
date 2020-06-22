d4e241a8333175e8638f11d05751a7561c64abd4
FJKhan <fjgenus@gmail.com>
2020-06-22 15:59:11 -0500

    fixes route call url

--- cypress/integration/todoActions/todoErrors.spec.js
+++ cypress/integration/todoActions/todoErrors.spec.js
@@ -1,16 +1,20 @@
 /// <reference  types= "cypress"/>
 
 describe('todo errors', () => {
-    it.only('displays error if getTodos fails', () => {
+    beforeEach(()=>{
+        cy.deleteTodos()
+    })
+    it('displays error if getTodos fails', () => {
         const error = `Could not get todos`
         cy.server()
         cy.route({
-            url: Cypress.env('API_URL'),
+            url: 'api/todos',
             method: 'GET',
             status: 500,
             response: { error: error}
-        })
+        }).as('loadPage')
         cy.visit('/')
+        cy.wait('@loadPage')
         cy.get('.todo-list').children().should('not.have.length.greaterThan', 0)
         cy.get('.error').should('be.visible')
     })
