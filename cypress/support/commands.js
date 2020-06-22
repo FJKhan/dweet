80566189192e97709fe39350e4e22432ecc62f8c
FJKhan <fjgenus@gmail.com>
2020-06-22 15:57:53 -0500

    Adds command for delete todos

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,3 +5,11 @@ Cypress.Commands.add('seedAndVisit', (seedData = 'fixture:todos') => {
     )
     cy.visit('/')
 })
+
+Cypress.Commands.add('deleteTodos', () => {
+    cy.request('GET', Cypress.env('API_URL'))
+        .its('body.todos')
+        .each((todo) =>
+            cy.request('DELETE', `${Cypress.env('API_URL')}/id/${todo._id}`)
+        )
+})
